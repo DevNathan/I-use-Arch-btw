1d74f4e305a0f916257e527ed1ed2f230d4ab1c3
Nathan <devnamho0910@gmail.com>
2025-04-08 12:09:08 +0900

    install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ set -e
 
 BIN_DIR="$HOME/.local/bin"
 TARGET="$BIN_DIR/archbtw"
-REPO_URL="https://github.com/DevNathan/I-use-Arch-btw/tree/main/archbtw"
+REPO_URL="https://raw.githubusercontent.com/DevNathan/I-use-Arch-btw/main/archbtw"
 
 mkdir -p "$BIN_DIR"
 
